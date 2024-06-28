35b2c8066f0fbf84328257f20c5c47e4d337e462
Arpit Raj <113095887+arpitraj96@users.noreply.github.com>
2024-06-28 22:13:33 +0530

    Create LearnAbstraction.java

---
+++ Day_01_JAVA/LearnAbstraction.java
@@ -0,0 +1,25 @@
+// 5th Program
+public class {
+  public static void main(String[] args){
+    SBI sbi = new SBI(1000, 2, 2);
+    System.out.println("The rate of Interest is: "+sbi.getRateOfInterest());
+  }
+}
+abstract class RateOfInterest{
+  abstract double getRateOfInterest();
+}
+
+class SBI extends RateOfInterest{
+  int principle;
+  int time;
+  int rate;
+  SBI(int principle, int time, int rate){
+    this.principle = principle;
+    this.time = time;
+    this.rate = rate;
+  }
+  @Override double getRateOfInterest(){
+    return (principle*rate*time)/100;
+  }
+
+}
